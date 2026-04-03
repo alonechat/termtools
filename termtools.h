d55e25c9ef107997e89039c27ca10ce5f3d91878
张小云 <160196654+hi-zcy@users.noreply.github.com>
2026-04-03 13:41:13 +0800

    feat(pixel engine): Refactor project structure, move pixel engine related files to termtools/pixel directory

--- termtools.h
+++ termtools.h
@@ -1,27 +1,27 @@
 #ifndef TERMTOOLS_H
 #define TERMTOOLS_H
 
-#include "termtools/core/formatting/color.h"
-#include "termtools/core/output/console.h"
-#include "termtools/core/formatting/style.h"
-#include "termtools/core/input/keyboard.h"
-#include "termtools/core/input/prompt.h"
 #include "termtools/components/logger/logger.h"
 #include "termtools/components/logger/status.h"
 #include "termtools/components/markdown/markdown.h"
+#include "termtools/components/panel/panel.h"
 #include "termtools/components/progress/progress.h"
+#include "termtools/components/table/table.h"
+#include "termtools/core/formatting/color.h"
+#include "termtools/core/formatting/style.h"
 #include "termtools/core/formatting/text.h"
+#include "termtools/core/input/keyboard.h"
+#include "termtools/core/input/prompt.h"
+#include "termtools/core/output/console.h"
+#include "termtools/pixel/pixel.h"
 #include "termtools/ui/codeblock/codeblock.h"
 #include "termtools/ui/columns/columns.h"
 #include "termtools/ui/divider/divider.h"
 #include "termtools/ui/group/group.h"
 #include "termtools/ui/heading/heading.h"
 #include "termtools/ui/list/list.h"
-#include "termtools/components/panel/panel.h"
 #include "termtools/ui/quote/quote.h"
-#include "termtools/components/table/table.h"
 #include "termtools/utils/utils.h"
-#include "pixel/pixel.h"
 
 namespace termtools {}
 
